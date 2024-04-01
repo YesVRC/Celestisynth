86a5e7298405d85c3a99a34bc0d990557e1e079b
Meme Man <81907436+RaveTr@users.noreply.github.com>
2024-04-01 11:43:07 +0200

    Afflict this skill issue

--- src/main/java/com/aqutheseal/celestisynth/api/skill/IDatapackableSkillInstance.java
+++ src/main/java/com/aqutheseal/celestisynth/api/skill/IDatapackableSkillInstance.java
@@ -1,9 +1,5 @@
 package com.aqutheseal.celestisynth.api.skill;
 
-import com.google.gson.JsonObject;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-
 public interface IDatapackableSkillInstance extends ISkillInstance {
 
 }
